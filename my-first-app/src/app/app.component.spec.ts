6fb61b49a014c3ab7ac8748e697ad37eaf40ee16
David Stirn <davidmstirn@gmail.com>
2023-09-11 16:09:18 -0400

    Fix failing test after updating AppComponent

--- my-first-app/src/app/app.component.spec.ts
+++ my-first-app/src/app/app.component.spec.ts
@@ -22,6 +22,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-first-app app is running!');
+    expect(compiled.querySelector('div h1')?.textContent).toContain('Welcome to my-first-app!');
   });
 });
