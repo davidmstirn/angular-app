e83b5186f0d60e59ecdfbc91da7f8ebe152ca653
David Stirn <davidmstirn@gmail.com>
2023-09-25 17:56:01 -0400

    Revert "Directives Pt 1"
    This reverts commit c5ebff43392540da9acdb1d106c6f982b9d3bca8.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,21 +1,14 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BasicHighlightDirective } from './directives/basic-highlight/basic-highlight.directive';
-import { BetterHighlightDirective } from './directives/better-highlight/better-highlight.directive';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    BasicHighlightDirective,
-    BetterHighlightDirective
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
