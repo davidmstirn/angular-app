ee9634dec39e0f41bc9724b22554fded5adf0326
David Stirn <davidmstirn@gmail.com>
2023-10-11 16:24:00 -0400

    Custom observables, pipes, and subjects

--- my-first-app/src/app/home/home.component.ts
+++ my-first-app/src/app/home/home.component.ts
@@ -1,15 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription, } from "rxjs";
+import {filter, map,} from "rxjs/operators";
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
+  private firstObsSubscription: Subscription;
 
   constructor() { }
 
   ngOnInit() {
+    // this.firstObsSubscription = interval(1000).subscribe(count=>{
+    //   console.log(count);
+    // })
+    const customIntervalObservable = Observable.create((observer)=>{
+      let count = 0;
+      setInterval(() => {
+        observer.next(count);
+        if(count === 2) {
+          observer.complete();
+        }
+        if(count > 3) {
+          observer.error(new Error('Count is greater than 3!'));
+        }
+        count++;
+      },1000)
+    });
+    this.firstObsSubscription = customIntervalObservable
+      .pipe(
+        map((data: number) => 'Round ' + (data + 1)),
+        filter((data: string) => data.includes('3'))
+      )
+      .subscribe((data: string)=>{
+        console.log(data);
+      }, error => {
+        console.error(error);
+      }, () => {
+        console.log("Completed!")
+      })
+  }
+
+  ngOnDestroy() {
+    this.firstObsSubscription.unsubscribe();
   }
 
 }
