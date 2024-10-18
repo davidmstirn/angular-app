b2f561a650475bb91efc76495e921b9610c2f9fb
David Stirn <davidmstirn@gmail.com>
2024-10-18 14:27:18 -0400

    Section baseline

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 
-import { DefaultComponent } from './default/default.component';
-import {SignalsComponent} from "./signals/signals.component";
+import { CounterOutputComponent } from './counter-output/counter-output.component';
+import { CounterControlsComponent } from './counter-controls/counter-controls.component';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    standalone: true,
-    imports: [DefaultComponent, SignalsComponent]
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  standalone: true,
+  imports: [CounterOutputComponent, CounterControlsComponent],
 })
 export class AppComponent {}
