606afaa53a94f729ea9c0c7dea3fef969e6c8306
David Stirn <davidmstirn@gmail.com>
2024-10-22 09:59:17 -0400

    Section baseline

--- my-first-app/src/app/app.component.ts
+++ my-first-app/src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
+  list = ['Milk', 'Sugar', 'Bread'];
+
+    onAdd(item) {
+      this.list.push(item);
+    }
 }
